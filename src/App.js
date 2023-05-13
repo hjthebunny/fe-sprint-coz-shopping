b3e1db4c3a092098d4fc26f77fb98014bbaca0f0
hjthebunny <monicahyunji@gmail.com>
2023-05-14 05:22:34 +0900

    react 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
