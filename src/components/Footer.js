e628c28393b8c4c724ab6b94969d111caf255629
hjthebunny <monicahyunji@gmail.com>
2023-05-15 03:39:02 +0900

    styled components 리팩토링

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,34 @@
-import React from "react";
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+//header와의 영역 분리 위한 임시 여백
+ margin-top: 50px;
+ 
+  box-sizing: border-box;
+  position: relative;
+  height: 58px;
+  background: #FFFFFF;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  >div{
+    text-align: center;
+  font-size: 12px;
+  color: #888888;
+  margin-top: 11px;
+  margin-bottom: 11px;
+  }
+`;
+
 
 
 function Footer() {
     return (
-        <footer>
-            <div id="footer_content">
+        <FooterContainer>
+            <div>
                 개인정보 처리방침 | 이용 약관<br />
                 All rights reserved @ Codestates
             </div>
 
-        </footer>
+        </FooterContainer>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,63 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const TitleContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 80px;
+  background: #FFFFFF;
+  box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+`;
+
+export const Title = styled.div`
+ display: flex;
+  align-items: center;
+  border: 1px solid gray;
+  cursor: pointer;
+>img{
+position: absolute;
+left: 5.94%;
+right: 89.77%;
+}
+>h1{
+    position: absolute;
+  left: 11.17%;
+  right: 70.86%;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  color: black;
+}
+`;
+
+export const MenuBtn = styled.img`
+ width: 30px;
+  height: 20px;
+  position: absolute;
+  left: 90%;
+  right: 5.88%;
+  cursor: pointer;
+`;
+
+
 
 function Header() {
     return (
         <header>
-            <Link to="/">
-                <div id="title">
+            <TitleContainer>
+                <Title>
                     <img id="logo" src="../logo.png" alt="logo" />
                     <h1 id="name">COZ Shopping</h1>
-                </div>
-            </Link>
-            <img id="menu" src="../menu.png" alt="hamburger menu" />
+                </Title>
+            </TitleContainer>
 
-
+            <MenuBtn id="menu" src="../menu.png" alt="hamburger menu" />
         </header>
+
+
     )
 }
 
