d59f8352a448a9c9c70201a646c33b92fc9753de
hjthebunny <monicahyunji@gmail.com>
2023-05-18 03:29:01 +0900

    css 수정

--- src/pages/ProductListPage/style.js
+++ src/pages/ProductListPage/style.js
@@ -1,27 +1,29 @@
 
 import styled from 'styled-components'
 
-export const ProductList = styled.ul`
+export const ProductListAll = styled.ul`
 display:flex;
 justify-content:center;
 list-style:none;
-
 margin: 26.5px 130px ;
 flex-wrap: wrap;
+`
 
+export const ProductList = styled(ProductListAll)`
+//마지막 열의 아이템이 하나 남은 경우 css 정렬을 위한 처리
 >li:last-child {
-  margin-right: auto;
-  margin-left: 106px;
+  margin-right: auto ;
+  margin-left:106px ; 
 }
 `
 
 export const TabWrapper = styled.div`
-display:flex;
-justify-content:center;`;
+display: flex;
+justify-content: center; `;
 
 export const TabMenu = styled.ul`
 
-margin-top:24px;
+margin-top: 24px;
 color: rgba(73, 73, 73, 0.5);
 font-weight: bold;
 display: flex;
@@ -29,14 +31,14 @@ gap: 36px;
 justify-content: center;
 list-style: none;
 cursor: pointer;
->li{
+> li{
     display: flex;
     flex-direction: column;
-   align-items: center;
+    align-items: center;
 }
-  `
+`
 export const TabText = styled.div`
-margin-top:6.5px ;
+margin-top: 6.5px;
 font-family: 'Inter';
 font-style: normal;
 font-weight: 400;
@@ -46,9 +48,10 @@ display: flex;
 align-items: center;
 text-align: center;
 color:${(props) => props.focused ? '#412DD4' : '#000000'};
-border-bottom: ${(prop) => prop.focused ? '2px solid #412DD4' : null};
-  `
+border-bottom: ${(props) => props.focused ? '3px solid #412DD4' : null};
+transition:0.3s
+`
 export const Desc = styled.div`
-display:flex;
-justify-content:center;
+display: flex;
+justify-content: center;
 `;
