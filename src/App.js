c065a1737ae4e2d8e4a3b017de13b70ff1ba1fb6
hjthebunny <monicahyunji@gmail.com>
2023-05-30 01:50:01 +0900

    lazy, Suspense 적용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header';
 import Footer from "./components/Footer/Footer";
-import Main from "./pages/Main/Main";
-import ProductListPage from "./pages/ProductListPage/ProductListPage";
-import Bookmark from "./pages/Bookmark/Bookmark";
+
 import './App.css';
+
+const Main = lazy(() => import('./pages/Main/Main'));
+const ProductListPage = lazy(() => import('./pages/ProductListPage/ProductListPage'));
+const Bookmark = lazy(() => import('./pages/Bookmark/Bookmark'));
 
 function App() {
 
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/products/list' element={<ProductListPage />} />
-        <Route path='/bookmark' element={<Bookmark />} />
-      </Routes>
+      <Suspense fallback={(<div className="loading">Loading...</div>)}>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/products/list' element={<ProductListPage />} />
+          <Route path='/bookmark' element={<Bookmark />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
